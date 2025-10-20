61c6f060bbb50c499f9f372a831f310f8c106fb2
rkoumproglou <rkoumproglou@gmail.com>
2025-10-20 14:59:09 +0200

    Update convert_app.py

--- convert_app.py
+++ convert_app.py
@@ -1,6 +1,7 @@
 from shiny import App, ui, render, reactive
 import pandas as pd
 import io
+import re
 
 # ---- Helper function: convert genotypes ----
 def convert_genotypes(df: pd.DataFrame) -> pd.DataFrame:
@@ -15,51 +16,30 @@ def convert_genotypes(df: pd.DataFrame) -> pd.DataFrame:
                 new_col.append("X")
                 continue
 
-            # Clean genotype string
-            val_clean = (
-                str(val)
-                .replace("/", "")
-                .replace("-", "")
-                .replace("|", "")
-                .strip()
-                .upper()
-            )
+            # Clean genotype string (remove separators)
+            val_clean = re.sub(r"[/|\-]", "", str(val).strip().upper())
 
-            p1 = str(parent1[i]).strip().upper()
-            p2 = str(parent2[i]).strip().upper()
+            # Extract parental alleles (e.g., AA -> A)
+            p1_alleles = list(set(re.sub(r"[/|\-]", "", parent1[i].strip().upper())))
+            p2_alleles = list(set(re.sub(r"[/|\-]", "", parent2[i].strip().upper())))
 
-            # Clean parental alleles (remove separators if any)
-            p1_clean = p1.replace("/", "").replace("-", "").replace("|", "")
-            p2_clean = p2.replace("/", "").replace("-", "").replace("|", "")
-
-            # Handle missing parents
-            if p1_clean == "" or p2_clean == "":
+            # Handle missing or invalid parents
+            if len(p1_alleles) == 0 or len(p2_alleles) == 0:
                 new_col.append("X")
                 continue
 
-            # Sort alleles so order doesn’t matter (AT == TA, 230200 == 200230)
-            val_sorted = "".join(sorted(val_clean))
-            p1_sorted = "".join(sorted(p1_clean))
-            p2_sorted = "".join(sorted(p2_clean))
+            # Offspring alleles
+            val_alleles = list(set(val_clean))
 
-            # Distinguish heterozygotes (H)
-            # If offspring genotype contains both parental alleles, mark as H
-            if p1_clean != p2_clean and all(allele in val_clean for allele in (p1_clean, p2_clean)):
+            # Heterozygote if offspring has one allele from each parent
+            if any(a in val_alleles for a in p1_alleles) and any(a in val_alleles for a in p2_alleles) and p1_alleles != p2_alleles:
                 new_col.append("H")
-
-            # Homozygotes for parent 1 or parent 2
-            elif val_sorted == p1_sorted:
+            # Homozygote for parent 1
+            elif all(a in p1_alleles for a in val_alleles):
                 new_col.append("A")
-            elif val_sorted == p2_sorted:
+            # Homozygote for parent 2
+            elif all(a in p2_alleles for a in val_alleles):
                 new_col.append("B")
-
-            # If partial match (e.g., one allele)
-            elif any(allele in val_clean for allele in p1_clean) and not any(allele in val_clean for allele in p2_clean):
-                new_col.append("A")
-            elif any(allele in val_clean for allele in p2_clean) and not any(allele in val_clean for allele in p1_clean):
-                new_col.append("B")
-
-            # Unrecognized genotype
             else:
                 new_col.append("X")
 
@@ -77,15 +57,15 @@ app_ui = ui.page_fluid(
         ui.HTML(
             "Paste your tab-separated data here<br>"
             "<span style='font-size: 0.9em; color: gray;'>"
-            "(The 1st column is the Marker_ID, 2nd and 3rd columns are the genotypes of parent P1 and P2)"
+            "(1st column: Marker ID, 2nd and 3rd columns: parental genotypes P1 and P2)"
             "</span>"
         ),
         "",
         rows=10,
-        placeholder='''Marker_ID\tP1\tP2\tInd1\tInd2\tInd3\tInd4\tInd5
-SSR1\t230\t200\t230\t230/200\t230/200\t230\t200
-SNP1\tA\tT\tA\tA/T\tT/A\tA\tT
-SNP2\tA\tC\tA\tAC\tCC\tC\tAA''',
+        placeholder='''SNP\tP1\tP2\tInd1\tInd2\tInd3\tInd4
+Gm01_138835\tAA\tGG\tAA\tAG\tAG\tAG
+Gm01_565991\tAA\tCC\tAA\tAC\tAC\tAC
+Gm01_901691\tCC\tTT\tCC\tTC\tTC\tTC''',
         width="100%",
     ),
 
@@ -138,6 +118,8 @@ def server(input, output, session):
 
 # ---- Run App ----
 app = App(app_ui, server)
+
+
 
 
 
